ef3295ff5113f0ea7cb68eaa0b2d9fa048f7a24f
anoopaneesh <anoopaneesh808@gmail.com>
2021-04-17 19:30:26 +0530

    added edit teacher page

--- helpers/adminHelper.js
+++ helpers/adminHelper.js
@@ -1,7 +1,7 @@
 var db = require('../config/config')
 var collection = require('../config/collections')
 var bcrypt = require('bcrypt')
-
+var objectId = require('mongodb').ObjectID
 
 module.exports = {
     addTeacher : (data) => {
@@ -9,6 +9,25 @@ module.exports = {
             data.password = await bcrypt.hash(data.dob,10)  
             db.get().collection(collection.TEACHER_COLLECTION).insertOne(data).then((response)=>{
                 resolve(response.ops[0])
+            }).catch(err=>{
+                reject(err)
+            })
+        })
+    },
+    getTeachers : () => {
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.TEACHER_COLLECTION).find().toArray().then((response)=>{
+                resolve(response)
+            }).catch(err=>{
+                reject(err)
+            })
+
+        })
+    },
+    getTeacher:(teacherId) => {
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.TEACHER_COLLECTION).findOne({_id:objectId(teacherId)}).then((response)=>{
+                resolve(response)
             }).catch(err=>{
                 reject(err)
             })
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var adminHelper = require('../helpers/adminHelper')
+var mailer = require('../config/mail-config')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('admin/dashboard', { admin: true });
@@ -11,10 +12,39 @@ router.get('/add-teacher',(req,res)=>{
 })
 
 router.post('/add-teacher',(req,res)=>{
-  console.log(req.body)
   adminHelper.addTeacher(req.body).then((data)=>{
-    console.log(data)
-    res.redirect('/admin')
+    var mailOptions = {
+      from: 'anooppk265@gmail.com',
+      to: data.email,
+      subject: 'You have been registered as a teacher at Eclass Management',
+      text: `Your Teacher id is : ${data.email} Your password is : ${data.dob}`
+    };
+    mailer.get().sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    let image = req.files.image
+    image.mv('./public/teacher-img/'+data._id+'.jpg',(err,done)=>{
+      if(err){
+        console.log(`Image file moving error : ${err}`)
+      }else{
+        res.redirect('/admin')
+      }
+    })  
+  })
+})
+router.get('/view-teachers',(req,res)=>{
+  adminHelper.getTeachers().then((teachers)=>{
+    res.render('admin/view-teachers',{admin:true,teachers})
+  })
+})
+router.get('/edit-teacher/:id',(req,res)=>{
+  adminHelper.getTeacher(req.params.id).then((teacher)=>{
+    console.log(teacher)
+    res.render('admin/edit-teacher',{admin:true,teacher})
   })
 })
 
